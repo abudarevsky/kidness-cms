c474fd0d59d6d946b7357a4339aa5f5654c9b111
kisele <catyshka@gmail.com>
2012-01-01 16:11:52 +0400

    video gallery remplate added

--- scrollable_parts/__init__.py
+++ scrollable_parts/__init__.py
@@ -35,6 +35,7 @@ def install():
     register_template(name = "Scrollable Overview", path="lfc/templates/scrollable_overview.html")
     register_template(name = "Carousel Overview", path="lfc/templates/jcarousel_overview.html")
     register_template(name = "Kidness Gallery", path="lfc/templates/jcarousel_gallery.html")
+    register_template(name = "Video Gallery", path="lfc/templates/video_gallery.html")
 #    register_template(name = "Scrollable Container", path="lfc/templates/scrollable_container.html")
 #    register_template(name = "Scrollable Part", path="lfc/templates/scrollable_part.html")
 
@@ -52,7 +53,7 @@ def install():
     register_content_type(Page,
         name="Page",
         sub_types=["Page"],
-        templates=["Article", "Plain", "Gallery", "Overview", "Scrollable Overview","Carousel Overview", "Kidness Gallery"],
+        templates=["Article", "Plain", "Gallery", "Overview", "Scrollable Overview","Carousel Overview", "Kidness Gallery", "Video Gallery"],
         default_template="Article")
 
     # Register Blog as a sub type of Page
